582ed8200f9bddc048f62af64c3e7713782f76d5
chamodxxc <nimsarawanas.inhe@gmail.com>
2026-05-23 13:19:19 +0530

    Modify bot configuration values
    Updated bot configuration with new token, owner ID, timezone, owner name, and artist name.

--- config.js
+++ config.js
@@ -8,16 +8,16 @@ require('dotenv').config({
 });
 
 const config = {
-  token: process.env.BOT_TOKEN || "", // Your bot token
-  owner_id: process.env.OWNER_ID || "", // Your telegram chat id
+  token: process.env.BOT_TOKEN || "8467583009:AAG-2FjRvTWuCLxM-TsUoWIsMnta_Z4Q4DU", // Your bot token
+  owner_id: process.env.OWNER_ID || "7330881176", // Your telegram chat id
   prefix: process.env.PREFIX || "/", // Your preferred prefix
   apiKey: process.env.API_KEY || "gifted", //Replace with your unlimited/paid apikey
   botName: process.env.BOT_NAME || "GIFTED-MD",
-  timezone: process.env.TIMEZONE || "Africa/Nairobi",
-  ownerName: process.env.OWNER_NAME || "Maurice Gift",
-  ownerUsername: process.env.OWNER_USERNAME || "mauricegift",
+  timezone: process.env.TIMEZONE || "Asia/Colombo",
+  ownerName: process.env.OWNER_NAME || "Chamod Nimsara",
+  ownerUsername: process.env.OWNER_USERNAME || "whiteshadow",
   apiUrl: process.env.API_URL || "https://api.gifted.co.ke", // Can replce with yours
-  artistName: process.env.ARTIST_NAME || "Powered by Gifted Apis",
+  artistName: process.env.ARTIST_NAME || "Powered by whiteshadow-md",
   url: process.env.URL || "https://gitcdn.gifted.co.ke/image/AZO_image.jpg",
   sourceUrl: process.env.SOURCE_URL || "https://github.com/mauricegift/telegram-bot",
 
